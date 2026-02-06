84fdafe92fd20bf51e70f7f92caa2ad3ea40386d
srushtigm-18 <srushtisrushtigm@gmail.com>
2026-02-06 12:36:13 +0530

    basic files on variables

--- Ipl.java
+++
@@ -1,6 +0,0 @@
-class Ipl{
-public static void main(String[] ipl){
-String iplTeams[]={"RCB","CSK","MI","SRH","RR","PBKS","KKR","DC","GT","LSG"};
-System.out.println(iplTeams);
- }
-}
---
+++ Mouse.java
@@ -0,0 +1,16 @@
+class Mouse{
+	static String productName="Mouse";
+	static String brandName="Razer";
+	static int colour="Black";
+	static String connectivityTechnology="Wireless";
+    static String specialFeatures="Economic Design";
+    static String movementDetectionTechnology="Optical";
+public static void main(String[] args){
+System.out.println("Product Name: "+productName);
+System.out.println("Brand Name: "+brandName);
+System.out.println("Product Colour: "+colour);
+System.out.println("Connrctivity technologies: "+connectivityTechnology);
+System.out.println("Special Features: "+specialFeatures);
+System.out.println("Type of movement Detection technology: "+movementDetectionTechnology);
+}
+}
